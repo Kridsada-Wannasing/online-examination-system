62b7250f1b5bd26fc017cb599c2c8e96dbff1a55
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-22 16:39:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config({
+  path: "./config.env",
+});
+
+const db = require("./models");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan());
+}
+
+db.sequelize.sync().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`App running on port ${process.env.PORT}`);
+  });
+});
