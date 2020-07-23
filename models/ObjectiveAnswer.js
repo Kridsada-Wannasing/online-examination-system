14e0c77cb24bde4ae0179dabf498d78cd92624d4
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-23 10:35:05 +0700

    add attributes to objectiveanswer model

---
+++ models/ObjectiveAnswer.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, dataTypes) => {
+  const model = sequelize.define(
+    "ObjectiveAnswers",
+    {
+      objectiveAnswerId: {
+        type: dataTypes.STRING(20),
+        primary: true,
+      },
+      chioce1: {
+        type: dataTypes.STRING(255),
+      },
+      chioce2: {
+        type: dataTypes.STRING(255),
+      },
+      chioce3: {
+        type: dataTypes.STRING(255),
+      },
+      chioce4: {
+        type: dataTypes.STRING(255),
+      },
+      chioce5: {
+        type: dataTypes.STRING(255),
+      },
+    },
+    {
+      tableName: "objective_answers",
+    }
+  );
+
+  return model;
+};
