893539de7b6a2df2b6c061c586b1acc41137abc5
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-23 10:49:53 +0700

    relationships of subjectiveanswer model

---
+++ models/SubjectiveAnswer.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, dataTypes) => {
+  const model = sequelize.define(
+    "SubjectiveAnswer",
+    {
+      subjectiveAnswerId: {
+        type: dataTypes.STRING(20),
+        primary: true,
+      },
+      keyword1: {
+        type: dataTypes.STRING(255),
+      },
+      keyword2: {
+        type: dataTypes.STRING(255),
+      },
+      keyword3: {
+        type: dataTypes.STRING(255),
+      },
+    },
+    {
+      tableName: "subjective_answers",
+    }
+  );
+
+  model.belongsTo(models.SubjectiveQuestion, {
+    foriegnKey: "subjectiveQuestionId",
+  });
+
+  return model;
+};
