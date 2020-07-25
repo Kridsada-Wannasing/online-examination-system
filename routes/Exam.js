1fdae86fbdf2e37b388f9cf43d298d87de0e3796
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-25 20:35:22 +0700

    route get api

--- routes/Exam.js
+++ routes/Exam.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const examController = require("../controllers/Exam");
 
 router.post("/create-exam", examController.createExam);
+router.get("/all", examController.getAllExam);
+router.get(":examId", examController.getExam);
 
 module.exports = router;
