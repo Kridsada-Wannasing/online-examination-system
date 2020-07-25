e4ecf03eec4ba9b0146326283e88f6f865a5d3dc
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-25 18:33:31 +0700

    get exam

--- controllers/Exam.js
+++ controllers/Exam.js
@@ -10,6 +10,33 @@ const createExam = async (req, res, next) => {
   });
 };
 
+const getAllExam = async (req, res, next) => {
+  const allExam = await db.Exam.findAll();
+
+  res.status(200).json({
+    status: "success",
+    allExam,
+  });
+};
+
+const getExam = async (req, res, next) => {
+  const target = await db.Exam.findOne({ where: { examId: req.body.examId } });
+
+  if (target) {
+    res.status(400).json({
+      status: "fail",
+      message: "ไม่มีชุดข้อสอบนี้",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    allExam,
+  });
+};
+
 module.exports = {
   createExam,
+  getAllExam,
+  getExam,
 };
