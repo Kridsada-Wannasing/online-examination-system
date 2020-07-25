288a2e9d643a5486e4654cddda3743fbdbb14266
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-25 12:31:02 +0700

    add attributes to teacher model

---
+++ models/Teacher.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, dataTypes) => {
+  const model = sequelize.define(
+    "Exam",
+    {
+      teacherId: {
+        type: dataTypes.INTEGER(4),
+        primaryKey: true,
+      },
+      firstName: {
+        type: dataTypes.STRING(50),
+      },
+      lastName: {
+        type: dataTypes.STRING(50),
+      },
+      email: {
+        type: dataTypes.STRING(50),
+      },
+      password: {
+        type: dataTypes.STRING(10),
+      },
+      faculty: {
+        type: dataTypes.STRING(100),
+      },
+      department: {
+        type: dataTypes.STRING(100),
+      },
+    },
+    {
+      tableName: "teachers",
+    }
+  );
+
+  return model;
+};
