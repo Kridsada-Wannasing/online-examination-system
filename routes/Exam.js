3dfceda85ad51d9cff5370b9eb30538dc6bafada
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-25 20:33:31 +0700

    route update api

--- routes/Exam.js
+++ routes/Exam.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const examController = require("../controllers/Exam");
 
 router.post("/create-exam", examController.createExam);
+router.patch("/edit-exam/:examId", examController.updateExam);
 
 module.exports = router;
