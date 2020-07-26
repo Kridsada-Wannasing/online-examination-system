a88c931e548ebc3d1eee2b1c6a752a6aadd7320d
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-26 19:14:20 +0700

    get tag api controller and route to app

--- controllers/Tag.js
+++ controllers/Tag.js
@@ -18,6 +18,35 @@ const createTag = async (req, res, next) => {
   });
 };
 
+const getAllTag = async (req, res, next) => {
+  const allTag = await db.Tag.findAll();
+
+  res.status(200).json({
+    status: "success",
+    allTag,
+  });
+};
+
+const getTag = async (req, res, next) => {
+  const target = await db.Tag.findOne({
+    where: { tagId: req.params.tagId },
+  });
+
+  if (!target) {
+    res.status(404).json({
+      status: "fail",
+      message: "ไม่มีชุดข้อสอบนี้",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    target,
+  });
+};
+
 module.exports = {
   createTag,
+  getAllTag,
+  getTag,
 };
--- routes/Tag.js
+++ routes/Tag.js
@@ -4,3 +4,7 @@ const router = express.Router();
 const tagController = require("../controllers/Tag");
 
 router.post("/create-tag", tagController.createTag);
+router.get("/all", tagController.getAllTag);
+router.get("/:tagId", tagController.getTag);
+
+module.exports = router;
