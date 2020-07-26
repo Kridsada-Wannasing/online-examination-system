d6074bb239fc090cc8d509325cb14a40355edb53
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-07-26 21:50:14 +0700

    get subject api and route to app

--- controllers/Subject.js
+++ controllers/Subject.js
@@ -10,6 +10,35 @@ const createSubject = async (req, res, next) => {
   });
 };
 
+const getAllSubject = async (req, res, next) => {
+  const allSubject = await db.Subject.findAll();
+
+  res.status(200).json({
+    status: "success",
+    allSubject,
+  });
+};
+
+const getSubject = async (req, res, next) => {
+  const target = await db.Subject.findOne({
+    where: { subjectId: req.params.subjectId },
+  });
+
+  if (!target) {
+    res.status(404).json({
+      status: "fail",
+      message: "ไม่มีรายวิชานี้",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    target,
+  });
+};
+
 module.exports = {
   createSubject,
+  getAllSubject,
+  getSubject,
 };
--- routes/Subject.js
+++ routes/Subject.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const subjectController = require("../controllers/Subject");
 
 router.post("/create-subject", subjectController.createSubject);
+router.get("/all-subject", subjectController.getAllSubject);
+router.get("/:subjectId", subjectController.getSubject);
 
 module.exports = router;
