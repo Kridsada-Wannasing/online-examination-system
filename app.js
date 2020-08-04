dcf4f2cad6fae0a11d0b9de7d2a773824e7399d6
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-04 11:43:00 +0700

    route to app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const db = require("./models");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const meetingRoute = require("./routes/Meeting");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan());
 }
+
+app.use("meeting", meetingRoute);
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
