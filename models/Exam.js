93bfc2255b06b197323960629cac7aa14db6e6bc
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-17 14:45:56 +0700

    edit relationship

--- models/Exam.js
+++ models/Exam.js
@@ -39,11 +39,11 @@ module.exports = (sequelize, dataTypes) => {
     model.hasMany(models.ExamLog, {
       foreignKey: "examId",
     });
-    model.hasMany(models.Subject, {
-      foreignKey: "examId",
-    });
     model.hasMany(models.Score, {
       foreignKey: "examId",
+    });
+    model.belongsTo(models.Subject, {
+      foreignKey: "subjectId",
     });
     model.belongsTo(models.Teacher, {
       foreignKey: "teacherId",
