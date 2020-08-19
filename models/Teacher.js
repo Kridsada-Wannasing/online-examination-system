9ed3399ecf807923335fcfac345c5b58b1395324
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-19 10:51:05 +0700

    teacher model

---
+++ models/Teacher.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, dataTypes) => {
+  const model = sequelize.define(
+    "Teacher",
+    {
+      teacherId: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: dataTypes.STRING(50),
+      },
+      lastName: {
+        type: dataTypes.STRING(50),
+      },
+      email: {
+        type: dataTypes.STRING(50),
+      },
+      password: {
+        type: dataTypes.STRING(10),
+      },
+      faculty: {
+        type: dataTypes.STRING(100),
+      },
+      department: {
+        type: dataTypes.STRING(100),
+      },
+    },
+    {
+      tableName: "teachers",
+    }
+  );
+
+  model.associate = (models) => {
+    model.hasMany(models.Exam, {
+      foreignKey: "teacherId",
+    });
+    model.hasMany(models.Meeting, {
+      foreignKey: "meetingId",
+    });
+  };
+
+  return model;
+};
