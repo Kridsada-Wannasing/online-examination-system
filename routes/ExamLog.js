6c796fb9e8de223177d3220380844511c20ba083
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-21 18:51:02 +0700

    edit examlog router

--- routes/ExamLog.js
+++ routes/ExamLog.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 const examLogController = require("../controllers/ExamLog");
 
-router.post("/create-log", examLogController.createExamLog);
-router.get("/all", examLogController.getAllExamLog);
-router.get("/:examLogId", examLogController.getExamLog);
-router.patch("/edit-log/:examLogId", examLogController.updateExamLog);
-router.delete("/delete-log/:examLogId", examLogController.deleteExamLog);
+router.post("/:examId/:questionId", examLogController.createExamLog);
+router.get("/", examLogController.getAllExamLog);
+// router.get("/:examLogId", examLogController.getExamLog);
+
+// มี 2 middleware คือลบอันเก่าทิ้งแล้วสร้างใหม่
+router.patch(
+  "/:examId/:questionId",
+  examLogController.updateExamLog,
+  examLogController.createExamLog
+);
+
+// ลบข้อมูลการสอบในครั้งนั้น
+router.delete("/:examId", examLogController.deleteExamLog);
 
 module.exports = router;
