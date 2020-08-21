e9fe8624bb7164ea3528d4db680d92d00cad2911
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-21 16:07:37 +0700

    edit router

--- routes/Teacher.js
+++ routes/Teacher.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const teacherController = require("../controllers/Teacher");
+const examNestedRoute = require("./Exam");
 
+const auth = passport.authenticate("teacher-jwt", { session: false });
+
+router.post("/login", teacherController.login);
+
+router.use(auth);
 router.post("/register-one", teacherController.registerOne);
 router.post("/register-many", teacherController.registerMany);
-router.post("/login", teacherController.login);
+router.get("/me", teacherController.getMe);
+
+router.use("/exam", examNestedRoute);
 
 module.exports = router;
