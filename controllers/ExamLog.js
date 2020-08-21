a8bd82ad86522e6ac2264f6c8d97b3f060b42bb3
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-21 17:32:27 +0700

    edit examlog controller

--- controllers/ExamLog.js
+++ controllers/ExamLog.js
@@ -1,16 +1,27 @@
 const db = require("../models");
 
+const mapArrayOfObject = function (arr, examId, questionId, studentId) {
+  return arr.map((obj) => ({
+    ...obj,
+    examId,
+    questionId,
+    studentId,
+  }));
+};
+
 const createExamLog = async (req, res, next) => {
-  await db.ExamLog.create({
-    ...req.body,
-    examId: req.params.examId,
-    questionId: req.params.questionId,
-    //passport ของ student เก็บใน req.user.studentId
-    studentId: req.user.studentId,
-  });
+  const result = mapArrayOfObject(
+    req.body,
+    req.params.examId,
+    req.params.questionId,
+    req.user.studentId
+  );
+
+  const newExamlog = await db.ExamLog.bulkCreate(result);
 
   res.status(201).json({
     status: "success",
+    newExamlog,
   });
 };
 
@@ -29,33 +40,36 @@ const getAllExamLog = async (req, res, next) => {
   });
 };
 
-const getExamLog = async (req, res, next) => {
-  const target = await db.ExamLog.findOne({
-    where: { examLogId: req.params.examLogId },
-  });
+// const getExamLog = async (req, res, next) => {
+//   const target = await db.ExamLog.findOne({
+//     where: { examLogId: req.params.examLogId },
+//   });
 
-  if (!target) {
-    res.status(404).json({
-      status: "fail",
-      message: "ยังไม่มีการตอบในข้อนี้",
-    });
-  }
+//   if (!target) {
+//     res.status(404).json({
+//       status: "fail",
+//       message: "ยังไม่มีการตอบในข้อนี้",
+//     });
+//   }
 
-  res.status(200).json({
-    status: "success",
-    target,
-  });
-};
+//   res.status(200).json({
+//     status: "success",
+//     target,
+//   });
+// };
 
 const updateExamLog = async (req, res, next) => {
-  await db.ExamLog.update(req.body, {
-    where: { examLogId: req.params.examLogId },
+  await db.ExamLog.destroy({
+    where: {
+      examId: req.params.examId,
+      //passport ของ student เก็บใน req.user.studentId
+      questionId: req.params.questionId,
+      studentId: req.user.studentId,
+    },
   });
 
-  res.status(200).json({
-    status: "succes",
-    message: "เปลี่ยนแปลงข้อมูลชุดข้อสอบนี้สำเร็จ",
-  });
+  //ไปยัง middleware createExamLog
+  next();
 };
 
 const deleteExamLog = async (req, res, next) => {
