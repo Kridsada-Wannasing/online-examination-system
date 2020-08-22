3a137d73a8a4af4c9e9000657a78352689893d90
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-22 18:01:03 +0700

    edit router name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require("./models");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const meetingRoute = require("./routes/Meeting");
+const meetingRoutes = require("./routes/Meeting");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan());
 }
 
-app.use("/meeting", meetingRoute);
+app.use("/meeting", meetingRoutes);
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
