816053e9311c4222f55821635209bcbff0dbcd32
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-22 21:52:30 +0700

    nested routes in teacherRoutes

--- routes/Teacher.js
+++ routes/Teacher.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const passport = require("passport");
 
 const teacherController = require("../controllers/Teacher");
-const examNestedRoute = require("./Exam");
+
+const examNestedRoutes = require("./Exam");
+const questionNestedRoutes = require("./Question");
+const tagNestedRoutes = require("./Tag");
+const subjectNestedRoutes = require("./Subject");
+const meetingNestedRoutes = require("./Subject");
 
 const auth = passport.authenticate("teacher-jwt", { session: false });
 
@@ -14,6 +19,10 @@ router.post("/register-one", teacherController.registerOne);
 router.post("/register-many", teacherController.registerMany);
 router.get("/me", teacherController.getMe);
 
-router.use("/exam", examNestedRoute);
+router.use("/exam", examNestedRoutes);
+router.use("/question", questionNestedRoutes);
+router.use("/tag", tagNestedRoutes);
+router.use("/subject", subjectNestedRoutes);
+router.use("/meeting", meetingNestedRoutes);
 
 module.exports = router;
