272bcf5b840446b4ca6772e7c166a02167e46855
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-24 12:29:51 +0700

    try catch in function

--- controllers/Subject.js
+++ controllers/Subject.js
@@ -1,63 +1,92 @@
 const db = require("../models");
 
 const createSubject = async (req, res, next) => {
-  const newSubject = await db.Subject.create(req.body);
+  try {
+    const newSubject = await db.Subject.create(req.body);
 
-  res.status(201).json({
-    status: "success",
-    message: "สร้างรายวิชาสำเร็จ",
-    newSubject,
-  });
+    res.status(201).json({
+      status: "success",
+      message: "สร้างรายวิชาสำเร็จ",
+      newSubject,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 const getAllSubject = async (req, res, next) => {
-  const allSubject = await db.Subject.findAll();
+  try {
+    const allSubject = await db.Subject.findAll();
 
-  res.status(200).json({
-    status: "success",
-    allSubject,
-  });
+    res.status(200).json({
+      status: "success",
+      allSubject,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 const getSubject = async (req, res, next) => {
-  const target = await db.Subject.findOne({
-    where: { subjectId: req.params.subjectId },
-    include: {
-      model: db.Section,
-      required: true,
-    },
-  });
+  try {
+    const target = await db.Subject.findOne({
+      where: { subjectId: req.params.subjectId },
+      include: {
+        model: db.Section,
+        required: true,
+      },
+    });
 
-  if (!target) {
-    res.status(404).json({
+    res.status(200).json({
+      status: "success",
+      target,
+    });
+  } catch (error) {
+    res.status(400).json({
       status: "fail",
-      message: "ไม่มีรายวิชานี้",
+      error,
     });
   }
-
-  res.status(200).json({
-    status: "success",
-    target,
-  });
 };
 
 const updateSubject = async (req, res, next) => {
-  await db.Subject.update(req.body, {
-    where: { subjectId: req.params.subjectId },
-  });
+  try {
+    const updatedSubject = await db.Subject.update(req.body, {
+      where: { subjectId: req.params.subjectId },
+    });
 
-  res.status(200).json({
-    status: "succes",
-    message: "เปลี่ยนแปลงข้อมูลรายวิชานี้สำเร็จ",
-  });
+    res.status(200).json({
+      status: "succes",
+      message: "เปลี่ยนแปลงข้อมูลรายวิชานี้สำเร็จ",
+      updatedSubject,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 const deleteSubject = async (req, res, next) => {
-  await db.Subject.destroy({
-    where: { subjectId: req.params.subjectId },
-  });
+  try {
+    await db.Subject.destroy({
+      where: { subjectId: req.params.subjectId },
+    });
 
-  res.status(204).send();
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 module.exports = {
