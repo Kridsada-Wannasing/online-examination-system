e00800b1a8a89ba1a1b27b784a82190ff47e694b
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-24 12:11:08 +0700

    try catch in function

--- controllers/Exam.js
+++ controllers/Exam.js
@@ -2,83 +2,111 @@ const db = require("../models");
 const { Op } = require("sequelize");
 
 const createExamInSubject = async (req, res, next) => {
-  const newExam = await db.Exam.create({
-    subjectId: req.params.subjectId,
-    teacherId: req.user.teacherId,
-    ...req.body,
-  });
+  try {
+    const newExam = await db.Exam.create({
+      subjectId: req.params.subjectId,
+      teacherId: req.user.teacherId,
+      ...req.body,
+    });
 
-  res.status(201).json({
-    status: "success",
-    message: "สร้างชุดข้อสอบสำเร็จ",
-    newExam,
-  });
+    res.status(201).json({
+      status: "success",
+      message: "สร้างชุดข้อสอบสำเร็จ",
+      newExam,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 const getAllExam = async (req, res, next) => {
-  const allExam = await db.Exam.findAll({
-    //ชุดข้อสอบของตัวเองหรือชุดข้อสอบที่ให้สิทธิ์การเข้าถึงเป็น Public
-    where: {
-      [Op.or]: [{ teacherId: req.user.teacherId }, { authority: true }],
-    },
-  });
+  try {
+    const allExam = await db.Exam.findAll({
+      //ชุดข้อสอบของตัวเองหรือชุดข้อสอบที่ให้สิทธิ์การเข้าถึงเป็น Public
+      where: {
+        [Op.or]: [{ teacherId: req.user.teacherId }, { authority: true }],
+      },
+    });
 
-  res.status(200).json({
-    status: "success",
-    allExam,
-  });
+    res.status(200).json({
+      status: "success",
+      allExam,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 const getExam = async (req, res, next) => {
-  const target = await db.Exam.findOne({
-    where: { examId: req.params.examId },
-    include: {
-      model: db.QuestionExam,
+  try {
+    const target = await db.Exam.findOne({
+      where: { examId: req.params.examId },
       include: {
-        model: db.Question,
-        include: [
-          { model: db.Choice },
-          { model: db.Answer },
-          {
-            model: db.QuestionTag,
-            include: [db.Tag],
-          },
-        ],
+        model: db.QuestionExam,
+        include: {
+          model: db.Question,
+          include: [
+            { model: db.Choice },
+            { model: db.Answer },
+            {
+              model: db.QuestionTag,
+              include: [db.Tag],
+            },
+          ],
+        },
       },
-    },
-  });
+    });
 
-  if (!target) {
-    res.status(404).json({
+    res.status(200).json({
+      status: "success",
+      target,
+    });
+  } catch (error) {
+    res.status(400).json({
       status: "fail",
-      message: "ไม่มีชุดข้อสอบนี้",
+      error,
     });
   }
-
-  res.status(200).json({
-    status: "success",
-    target,
-  });
 };
 
 const updateExam = async (req, res, next) => {
-  await db.Exam.update(req.body, {
-    where: { examId: req.params.examId },
-  });
+  try {
+    const updatedExam = await db.Exam.update(req.body, {
+      where: { examId: req.params.examId },
+    });
 
-  res.status(200).json({
-    status: "succes",
-    message: "เปลี่ยนแปลงข้อมูลชุดข้อสอบนี้สำเร็จ",
-    updatedExam,
-  });
+    res.status(200).json({
+      status: "succes",
+      message: "เปลี่ยนแปลงข้อมูลชุดข้อสอบนี้สำเร็จ",
+      updatedExam,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 const deleteExam = async (req, res, next) => {
-  await db.Exam.destroy({
-    where: { examId: req.params.examId },
-  });
+  try {
+    await db.Exam.destroy({
+      where: { examId: req.params.examId },
+    });
 
-  res.status(204).send();
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
 
 module.exports = {
