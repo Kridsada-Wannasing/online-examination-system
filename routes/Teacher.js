412f33c3c1912543a58cf2ddde0ebd35d4dfbe52
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-29 17:06:01 +0700

    edit teacher route

--- routes/Teacher.js
+++ routes/Teacher.js
@@ -6,6 +6,8 @@ const teacherControllers = require("../controllers/Teacher");
 
 const examNestedRoutes = require("./Exam");
 const questionNestedRoutes = require("./Question");
+const answerNestedRoutes = require("./Answer");
+const choiceNestedRoutes = require("./Choice");
 const subjectNestedRoutes = require("./Subject");
 const tagNestedRoutes = require("./Tag");
 
@@ -17,11 +19,14 @@ router.use(auth);
 
 router.post("/register-one", teacherControllers.registerOne);
 router.post("/register-many", teacherControllers.registerMany);
+
 router.get("/me", teacherControllers.getMe);
 router.post("/updateMe", teacherControllers.updateMe);
 
 router.use("/exam", examNestedRoutes);
 router.use("/question", questionNestedRoutes);
+router.use("/choice", choiceNestedRoutes);
+router.use("/answer", answerNestedRoutes);
 router.use("/subject", subjectNestedRoutes);
 router.use("/tag", tagNestedRoutes);
 
