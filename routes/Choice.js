04ece780e1329b7b8a6a94b0b281b0c5060f5cd5
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-29 11:55:05 +0700

    add choice routes

---
+++ routes/Choice.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const choiceControllers = require("../controllers/Choice");
+
+router.get("/", choiceControllers.getAllChoice);
+router.post("/", choiceControllers.createChoice);
+router.patch("/:choiceId", choiceControllers.updateChoice);
+router.delete("/:choiceId", choiceControllers.deleteChoice);
+
+module.exports = router;
