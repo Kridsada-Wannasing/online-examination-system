db4cd783395f8eb8201b3527eee52879b4ae409d
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-08-29 10:44:23 +0700

    config admin passport and add admin route in app

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+const adminRoutes = require("./routes/Admin");
+
 require("./config/passport/Student");
 require("./config/passport/Teacher");
+require("./config/passport/Admin");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan());
 }
+
+app.use("/admin", adminRoutes);
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
